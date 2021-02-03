248e62cb89e9ca4968f2c2df0a902dc50a6c1536
netty-carroll <netty.carroll@live.com>
2021-02-03 11:31:55 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const list = document.getElementsByTagName("li");
-list.length;
-list[0]
-list[3]
-list[0].style.color="red";
-list[2].style.color="red"
+//list.length;
+//list[0]
+//list[3]
+//list[0].style.color="red";
+//list[2].style.color="red";
+const red = document.getElementsByClassName("not-orange")
+for (let i = 0; i< red.length; i++){
+    red[i].style.color = "red";
+}
