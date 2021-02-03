5acc5e625957e450062cf48fd826139d472bfc90
netty <netty.carroll@live.com>
2021-02-03 12:51:10 +0000

    4th activity

---
+++ mc-5-1-js-dom-starterfiles/4 - querySelector and querySelectorAll/app.js
@@ -0,0 +1,13 @@
+//document.querySelectorAll("li");
+//document.querySelector("li;");
+//document.querySelector("#heading");
+//document.querySelector(".list-parent");
+//document.querySelectorAll(".green")
+
+const listItems = document.querySelectorAll("li:nth-child(even");
+//console.log(listItems);
+//console.log(listItems.length);
+
+for (let i=0; i<listItems.length; i++){
+    listItems[i].getElementsByClassName.color = "lightgreen"
+}
