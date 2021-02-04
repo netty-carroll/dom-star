af90aa4299c757583bf89fc6270ae339c554fd3f
netty <netty.carroll@live.com>
2021-02-04 11:41:43 +0000

    8 app.js
    added hide button

--- 8/app.js
+++ 8/app.js
@@ -1,5 +1,6 @@
 const input = document.getElementById("input");
 const button = document.getElementById("submit");
+const hide = document.getElementById("hide");
 
 button.addEventListener("click", ()=> {
     let listItem = document.createElement("li");
@@ -7,6 +8,17 @@ button.addEventListener("click", ()=> {
   console.log(listItem)
   let list = document.getElementsByTagName("ul")[0];
     list.appendChild(listItem);
-    input.value = '';
+    input.value = "";
+
+    hide.addEventListener("click", ()=>{
+        let list = document.getElementsByTagName("ul")[0];
+        if(list.style.display == "none"){
+            list.style.display = "block";
+            hide.textContent = "show";
+        }else {
+            list.style.display = 'none';
+            hide.textContent = 'show';
+        }
+    })
 
 })
