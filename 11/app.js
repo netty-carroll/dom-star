ff6b7558b455fcb76bcab6eec7eb88bab80e92da
netty <netty.carroll@live.com>
2021-02-04 21:24:27 +0000

    mouseover/mouseout added

--- 11/app.js
+++ 11/app.js
@@ -1,7 +1,18 @@
 const input = document.getElementById("input");
 const button = document.getElementById("submit");
 const remove = document.getElementById("remove");
+const listItems = document.getElementsByTagName("li");
 
+for (let listitem of listItems){
+    listitem.addEventListener("mouseover", ()=>{
+        listitem.textContent = listitem.textContent.toUpperCase();
+    });
+
+    listitem.addEventListener("mouseout", ()=>{
+        listitem.textContent = listitem.textContent.toLowerCase();
+    });
+   
+}
 button.addEventListener('click', ()=>{
     let listitem = document.createElement("li");
     let list = document.getElementsByTagName("ul")[0];
